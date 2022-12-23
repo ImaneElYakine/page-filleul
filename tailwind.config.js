3b0295fb1403b000019568934d386bb4e11b24ca
Imane El Yakine <64931446+ImaneElYakine@users.noreply.github.com>
2022-12-23 16:31:01 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'index.html',
-    'test.html'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'fushia' : '#AD005E',
-        'dark-fushia': '#73003f',
-        'vert' : '#629D00'
-      },
-      fontFamily: {
-        'mediametrie': ['Mediametrie', 'sans-serif'],
-      },
-      screens: {
-        'lg': '1250px',
-        'md': '990px'
-      }
+    content: [
+        'index.html',
+        'test.html'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'fushia': '#AD005E',
+                'dark-fushia': '#73003f',
+                'vert': '#629D00',
+                'bleu': '#14A8C6',
+                'dark-bleu': '#0D7084'
+            },
+            fontFamily: {
+                'mediametrie': ['Mediametrie', 'sans-serif'],
+            },
+            screens: {
+                'lg': '1250px',
+                'md': '990px'
+            },
+            backgroundImage: {
+                'hero': "url('img/background.png')"
+            },
+            keyframes: {
+                slide: {
+                    '0%': { transform: 'translateX(-10px)', opacity: '0'},
+                    '100%': { transform: 'translateX(0px)' , opacity: '1'}
+                },
+                appear: {
+                    '0%': { opacity: '0'},
+                    '100%': { opacity: '1'}
+                },
+            },
+            animation: {
+                'slide-left': 'slide 1.5s linear',
+                'appear': 'appear 1.5s linear',
+            },
+
+        },
     },
-
-
-  },
-  plugins: [],
+    plugins: [],
 }
