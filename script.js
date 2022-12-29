cc924c397b532cd0a78ad1734cce54b6344c2463
Imane El Yakine <64931446+ImaneElYakine@users.noreply.github.com>
2022-12-29 13:45:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const modal = document.getElementById("myModal");
+const scroll = document.getElementById("scroll-down");
 
 displayModal();
 
 function displayModal() {
     modal.style.display = "block";
     document.body.style.overflow = 'hidden';
+    scroll.style.display = "none";
 }
 
 function hideModal() {
     modal.style.display = "none";
     document.body.style.overflow = 'visible';
+    scroll.style.display = "block";
+
 }
